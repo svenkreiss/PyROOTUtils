d6af6ede4d448eeccfff01a895102d16de6fbaa2
Sven Kreiss <sk@svenkreiss.com>
2014-01-16 16:58:42 +0100

    Fixing ModelConfigUtils.

--- PyROOTUtils/__init__.py
+++ PyROOTUtils/__init__.py
@@ -3,3 +3,5 @@ from utils import *
 from Graph import Graph
 from Legend import Legend
 from Marker import Marker,CrossMarker
+
+import ModelConfigUtils
--- example.py
+++ example.py
@@ -7,13 +7,14 @@ import PyROOTUtils.style
 container = []
 
 def main():
-	canvas = ROOT.TCanvas("c","c",400,300)
+	canvas = ROOT.TCanvas("c","c",600,450)
 	axes = canvas.DrawFrame( -90,-90,90,90 )
 	axes.GetXaxis().SetTitle( "x [km]" )
 	axes.GetYaxis().SetTitle( "y [km]" )
 
 	hLine = PyROOTUtils.DrawHLine( 0.0, lineStyle=ROOT.kDotted, lineWidth=1 )
-	bla = PyROOTUtils.DrawText( 0.2, 0.7, "This is just random text\nto show how easy it can be\nto write aligned multi-line text.", textSize=0.035)
+	v = PyROOTUtils.ModelConfigUtils.varsDictFromString('alpha=0.1')
+	bla = PyROOTUtils.DrawText( 0.2, 0.7, "This is just random text\nto show how easy it can be\nto write aligned multi-line text\nand how varsDictFromString('alpha=0.1') works: "+str(v), textSize=0.035)
 
 	l1 = PyROOTUtils.Legend( 0.2,0.2, textSize=0.035, valign="bottom" )
 	l1.AddEntry( hLine, "line at y=0", "L" )
